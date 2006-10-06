904486029af4fddf7b7766df2bcacfe83de5fff3
cestradac <cestradac@386180a9-1f19-0410-bc33-a93c7e5c1b3c>
2006-10-06 00:16:57 +0000

    Refactor (add-in support planing)

---
+++ code/PTM.Addin.WeekView/WeekReport.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PTM.Addin.WeekView
+{
+	public class WeekReport : PTM.Addin.TabPageAddin
+	{
+		private System.Windows.Forms.Label label1;
+		private System.ComponentModel.IContainer components = null;
+
+		public WeekReport()
+		{
+			// This call is required by the Windows Form Designer.
+			InitializeComponent();
+
+			// TODO: Add any initialization after the InitializeComponent call
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if (components != null) 
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(120, 88);
+			this.label1.Name = "label1";
+			this.label1.TabIndex = 0;
+			this.label1.Text = "prueba";
+			// 
+			// WeekReport
+			// 
+			this.Controls.Add(this.label1);
+			this.Name = "WeekReport";
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+	}
+}
+
