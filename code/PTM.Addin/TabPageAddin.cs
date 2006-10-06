db82917d6f42c6ca94d466b992278bea6f0f6315
cestradac <cestradac@386180a9-1f19-0410-bc33-a93c7e5c1b3c>
2006-10-06 00:16:38 +0000

    Refactor (add-in support planing)

---
+++ code/PTM.Addin/TabPageAddin.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Windows.Forms;
+
+namespace PTM.Addin
+{
+	/// <summary>
+	/// Summary description for TabPageAddin.
+	/// </summary>
+	public class TabPageAddin : UserControl
+	{
+		/// <summary> TabPageAddin
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		public TabPageAddin()
+		{
+			// This call is required by the Windows.Forms Form Designer.
+			InitializeComponent();
+
+			// TODO: Add any initialization after the InitializeComponent call
+
+		}
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Component Designer generated code
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// TabPageAddin
+			// 
+			this.Name = "TabPageAddin";
+			this.Size = new System.Drawing.Size(296, 280);
+		}
+		#endregion
+		
+		
+	}
+}
