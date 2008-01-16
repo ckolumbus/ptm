86afdbf875188bd2b45e38382561f69e40e4bc14
cestradac <cestradac@386180a9-1f19-0410-bc33-a93c7e5c1b3c>
2008-01-16 13:11:06 +0000

    Current week and refresh data buttons adder at the week report.

--- code/PTM.Addin.WeekView/WeekReport.cs
+++ code/PTM.Addin.WeekView/WeekReport.cs
@@ -16,6 +16,8 @@ namespace PTM.Addin.WeekView
 		private IContainer components = null;
         private ToolTip toolTip;
         private MonthCalendar monthCalendar;
+        private Button curWeekButton;
+        private Button refreshButton;
 		private int currentWeek;
 
 		public WeekReport()
@@ -28,7 +30,7 @@ namespace PTM.Addin.WeekView
 			this.dayView.ResolveAppointments+=new ResolveAppointmentsEventHandler(dayView_ResolveAppointments);
             this.dayView.MouseMove += new MouseEventHandler(dayView_MouseMove);
 			currentWeek = 0;
-			this.Status = String.Empty;
+            this.Status = "Ready";
 		}        
 
 		/// <summary>
@@ -54,14 +56,40 @@ namespace PTM.Addin.WeekView
 		private void InitializeComponent()
 		{
             this.components = new System.ComponentModel.Container();
+            Calendar.DrawTool drawTool3 = new Calendar.DrawTool();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WeekReport));
-            Calendar.DrawTool drawTool1 = new Calendar.DrawTool();
+            this.dayView = new Calendar.DayView();
             this.backButton = new System.Windows.Forms.Button();
             this.forwardButton = new System.Windows.Forms.Button();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.monthCalendar = new System.Windows.Forms.MonthCalendar();
-            this.dayView = new Calendar.DayView();
+            this.curWeekButton = new System.Windows.Forms.Button();
+            this.refreshButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // dayView
+            // 
+            drawTool3.DayView = this.dayView;
+            this.dayView.ActiveTool = drawTool3;
+            this.dayView.AllowInplaceEditing = false;
+            this.dayView.AllowNew = false;
+            this.dayView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dayView.DaysToShow = 7;
+            this.dayView.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.dayView.HalfHourHeight = 34;
+            this.dayView.Location = new System.Drawing.Point(186, 3);
+            this.dayView.Name = "dayView";
+            this.dayView.SelectionEnd = new System.DateTime(((long)(0)));
+            this.dayView.SelectionStart = new System.DateTime(((long)(0)));
+            this.dayView.Size = new System.Drawing.Size(190, 333);
+            this.dayView.StartDate = new System.DateTime(((long)(0)));
+            this.dayView.TabIndex = 0;
+            this.dayView.WorkingHourEnd = 23;
+            this.dayView.WorkingHourStart = 0;
+            this.dayView.WorkingMinuteEnd = 59;
+            this.dayView.WorkingMinuteStart = 0;
             // 
             // backButton
             // 
@@ -100,32 +128,30 @@ namespace PTM.Addin.WeekView
             this.monthCalendar.ShowTodayCircle = false;
             this.monthCalendar.TabIndex = 4;
             // 
-            // dayView
+            // curWeekButton
             // 
-            drawTool1.DayView = this.dayView;
-            this.dayView.ActiveTool = drawTool1;
-            this.dayView.AllowInplaceEditing = false;
-            this.dayView.AllowNew = false;
-            this.dayView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.dayView.DaysToShow = 7;
-            this.dayView.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
-            this.dayView.HalfHourHeight = 34;
-            this.dayView.Location = new System.Drawing.Point(186, 3);
-            this.dayView.Name = "dayView";
-            this.dayView.SelectionEnd = new System.DateTime(((long)(0)));
-            this.dayView.SelectionStart = new System.DateTime(((long)(0)));
-            this.dayView.Size = new System.Drawing.Size(190, 333);
-            this.dayView.StartDate = new System.DateTime(((long)(0)));
-            this.dayView.TabIndex = 0;
-            this.dayView.WorkingHourEnd = 23;
-            this.dayView.WorkingHourStart = 0;
-            this.dayView.WorkingMinuteEnd = 59;
-            this.dayView.WorkingMinuteStart = 0;
+            this.curWeekButton.Location = new System.Drawing.Point(11, 184);
+            this.curWeekButton.Name = "curWeekButton";
+            this.curWeekButton.Size = new System.Drawing.Size(92, 23);
+            this.curWeekButton.TabIndex = 5;
+            this.curWeekButton.Text = "Current Week";
+            this.curWeekButton.UseVisualStyleBackColor = true;
+            this.curWeekButton.Click += new System.EventHandler(this.curWeekButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(11, 225);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(92, 23);
+            this.refreshButton.TabIndex = 6;
+            this.refreshButton.Text = "Refresh Data";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
             // 
             // WeekReport
             // 
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.curWeekButton);
             this.Controls.Add(this.forwardButton);
             this.Controls.Add(this.backButton);
             this.Controls.Add(this.dayView);
@@ -144,6 +170,7 @@ namespace PTM.Addin.WeekView
 			try
 			{
 				Cursor.Current = Cursors.WaitCursor;
+			    this.Status = "Retrieving data...";
 				DateTime day = args.StartDate;
 				do
 				{
@@ -173,13 +200,10 @@ namespace PTM.Addin.WeekView
 					day = day.AddDays(1);
 				} while (day <= args.EndDate);
 			}
-			catch
-			{
-				throw;
-			}
 			finally
 			{
 				Cursor.Current = Cursors.Default;
+			    this.Status = "Ready";
 			}
 		}
 
@@ -219,13 +243,6 @@ namespace PTM.Addin.WeekView
 			}
 		}
 
-		protected override void OnKeyDown(KeyEventArgs e)
-		{
-			if(e.KeyCode == Keys.F5)
-				SetWeek(currentWeek);
-			base.OnKeyDown (e);
-		}
-
         void dayView_MouseMove(object sender, MouseEventArgs e)
         {
             if (dayView.GetAppointmentAt(e.X, e.Y) != null)
@@ -239,6 +256,17 @@ namespace PTM.Addin.WeekView
                 //toolTip.ToolTipTitle = "";
                 toolTip.SetToolTip(dayView, "");
             }
+        }
+
+        private void curWeekButton_Click(object sender, EventArgs e)
+        {
+            currentWeek = 0;
+            SetWeek(currentWeek);
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            SetWeek(currentWeek);
         }
         
         
