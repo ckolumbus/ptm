6fa6f716d7626a3143a4874558d2f7309418b4ea
cestradac <cestradac@386180a9-1f19-0410-bc33-a93c7e5c1b3c>
2008-06-30 01:23:56 +0000

    version set to 1.4.5

--- code/PTM/Framework/Helpers/ConfigurationHelper.cs
+++ code/PTM/Framework/Helpers/ConfigurationHelper.cs
@@ -91,7 +91,7 @@ namespace PTM.Framework.Helpers
 
 		public static string GetVersionString()
 		{
-			return "v. 1.4.3";
+			return "v. 1.4.5";
 		} //GetVersionString
 
 		public static string GetInternalVersionString()
