7209e44c1c7a1ef0b41a1577a0bb24cc747b93d8
CKolumbus <ckolumbus@ac-drexler.de>
2014-11-23 13:59:38 +0100

    update version to ckol.6b

--- code/PTM/Framework/Helpers/ConfigurationHelper.cs
+++ code/PTM/Framework/Helpers/ConfigurationHelper.cs
@@ -91,7 +91,7 @@ namespace PTM.Framework.Helpers
 
 		public static string GetVersionString()
 		{
-            string version = "v. 1.4.5-ckol.5b";
+            string version = "v. 1.4.5-ckol.6b";
 #if NO_APPSLOG
             version += " (NO AppsLog)";
 #endif
